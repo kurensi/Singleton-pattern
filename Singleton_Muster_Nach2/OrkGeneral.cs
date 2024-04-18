3ae9492b97f6aa01eb43b94a5ebd634631821729
Dejan Pantos <126320274+kurensi@users.noreply.github.com>
2024-04-18 16:39:03 +0200

    Add files via upload

---
+++ Singleton_Muster_Nach2/OrkGeneral.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton_Muster_Nach2
+{
+    class OrkGeneral
+    {
+        public string name;
+        private static OrkGeneral My_General=null;
+        private OrkGeneral()
+        {
+
+        }                        
+        public static OrkGeneral getInstance()
+        {
+            if (My_General == null)
+            {
+                My_General = new OrkGeneral();
+            }
+            return My_General;
+
+
+        }
+        public void setName(string General)
+        {
+            name = General;
+        }
+        public string AngrifsBefell()
+        {
+            return name + " Befehl : Jetzt greifen wir an !!!!!!";
+        }
+        public string RückzugsBefell()
+        {
+            return name + " Befehl : Jetzt ziehen  wir zurück !!!!!!";
+        }
+    }
+}
+
---
+++ Singleton_Muster_Nach2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Singleton_Muster_Nach2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrkGeneral King = OrkGeneral.getInstance();
+            King.setName("Ork_Malakas der I");
+            OrkGeneral King3 = OrkGeneral.getInstance();
+            King3.setName("Ork_Malakas der III");
+            OrkGeneral King2 = OrkGeneral.getInstance();
+            King2.setName("Ork_Malakas der II");
+           
+            Console.WriteLine(King.AngrifsBefell());
+            Console.WriteLine(King2.AngrifsBefell());
+            Console.WriteLine(King3.AngrifsBefell());
+            Console.WriteLine(King2.RückzugsBefell());
+            Console.WriteLine(King3.RückzugsBefell());
+            Console.WriteLine(King.RückzugsBefell());
+            Console.WriteLine($"Name des General:{King3.name}");
+           
+            Console.ReadKey();
+        }
+    }
+}
